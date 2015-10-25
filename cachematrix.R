f3e02e60f85d16d9c850eeba9fa2195059fa8127
Tetration Analytics <tetter@deepai-vm.tetrationanalytics.com>
2015-10-25 14:37:22 -0700

    Assignment 2 submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-## A
-makeCacheMatrix <- function(x = matrix()) {
+# Matrix inversion is usually a costly computation and there may be some
+# benefit to caching the inverse of a matrix rather than computing it
+# repeatedly.
 
+# makeCacheMatrix creates a list containing the following functions:
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of the inverse
+# 4. get the value of the inverse
+makeCacheMatrix <- function(x = matrix()) {
+	inverse <- NULL
+	# set the value of the matrix
+	set <- function(y) {
+		x <<- y
+		inverse <<- NULL  # Initialize inverse to NULL everytime 
+						  # a new matrix is set
+	}
+	# get the value of the matrix 
+	get <- function() x
+	# set the value of the inverse
+	setinverse <- function(inv) inverse <<- inv
+	# get the value of the inverse
+	getinverse <- function() inverse
+	list(set = set, get = get, 
+		 setinverse = setinverse, getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	inv <- x$getinverse()
+	if (!is.null(inv)) {  
+		# when inv is present in the cache, use it and don't recompute.
+		message("getting cached inverse")
+		return (inv)
+	}
+	# Else compute the inverse and cache it.
+	matr <- x$get()
+	inv <- solve(matr, ...)
+	x$setinverse(inv)
+	inv
 }
